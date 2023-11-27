be2dcedbf0b74dcc2b46d09fa3bc673fcd7a4d40
12imthi <110447839+12imthi@users.noreply.github.com>
2023-11-27 21:27:57 +0530

    Add files via upload

---
+++ odd(A)EvenNum.js
@@ -0,0 +1,12 @@
+// var result = document.getElementById('result');
+var num = 6;
+for(var i=1;i<=num;i++) {
+    if(i%2 == 0) {
+       console.log(`Even Number : ${i}`);
+    //    result.textContent = `Even Number : ${i}`;
+    }
+    else {
+        console.log(`Odd Number : ${i}`);
+        // result.textContent = `Odd Number : ${i}`;
+    }
+}
